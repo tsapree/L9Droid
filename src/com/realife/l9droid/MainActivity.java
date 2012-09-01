984b8d57506ac6d09f91e39b461764097c1186b6
tsapree <tsap@yandex.ru>
2012-09-02 02:14:13 +0600

    micro fix

--- src/com/realife/l9droid/MainActivity.java
+++ src/com/realife/l9droid/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity implements OnClickListener {
         return true;
     }
 
-	@Override
+	//@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.bCmd: // кнопка ввода команды
