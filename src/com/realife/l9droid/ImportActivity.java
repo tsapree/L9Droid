683b9fa068df92a0b26f6cf270fc32ec26357759
tsapree <tsap@yandex.ru>
2012-12-04 17:29:50 +0600

    ImportActivity, blank for now

---
+++ src/com/realife/l9droid/ImportActivity.java
@@ -0,0 +1,14 @@
+package com.realife.l9droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImportActivity extends Activity {
+
+	@Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.import_files);
+	};
+	
+}
--- src/com/realife/l9droid/LibraryActivity.java
+++ src/com/realife/l9droid/LibraryActivity.java
@@ -3,13 +3,17 @@ package com.realife.l9droid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class LibraryActivity extends Activity {
+public class LibraryActivity extends Activity implements OnMenuItemClickListener {
 	
 	ListView lvMain;
 	String[] paths;
@@ -50,4 +54,23 @@ public class LibraryActivity extends Activity {
 	    };
 	    
 	  }
+	
+    //@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuItem mi;
+    	mi = menu.add(0,1,0,"Import");
+    	mi.setOnMenuItemClickListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    public boolean onMenuItemClick(MenuItem arg0) {
+		switch (arg0.getItemId()) {
+		case 1: //library TODO: переделать в id, возможно перенести меню в ресурсы
+			Intent intent=new Intent(this, ImportActivity.class);
+			startActivityForResult(intent, 1); //TODO: "1"-change it or kill ))
+	        //mi.setIntent(intent);
+			break;
+		};
+		return false;
+	}
 }
