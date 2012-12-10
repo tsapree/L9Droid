29df6324af0daae0ad5aa98e3118669f46b0dc1f
tsapree <tsap@yandex.ru>
2012-12-11 01:56:14 +0600

    disable divider in log

--- src/com/realife/l9droid/MainActivity.java
+++ src/com/realife/l9droid/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends Activity implements OnClickListener,OnEditorAc
         
         // находим список
         lvMain = (ListView) findViewById(R.id.lvLog);
+        lvMain.setDividerHeight(0);
                 
         command=null;
         
